bd6f129f0b8b9b02a70f3b2f732c02fd721581cc
Crinax <agreedeth@gmail.com>
2024-11-03 22:53:01 +0300

    refactor(files): remove unused files

--- src/infrastructure/nestjs/app.module.ts
+++ src/infrastructure/nestjs/app.module.ts
@@ -5,17 +5,10 @@ import {
 } from 'src/infrastructure/nestjs-graphql';
 import { KnexProvider } from 'src/infrastructure/nestjs/adapters/knex/knex.provider';
 import { TaskGetByIdServiceProvider } from 'src/infrastructure/nestjs/adapters/services/tasks/task-get-by-id-service.provider';
-import { AppController } from 'src/infrastructure/nestjs/app.controller';
-import { AppService } from 'src/infrastructure/nestjs/app.service';
 
 @Module({
   imports: [defineGraphQlModule()],
-  controllers: [AppController],
-  providers: [
-    AppService,
-    KnexProvider,
-    TaskGetByIdServiceProvider,
-    ...defineResolvers(),
-  ],
+  controllers: [],
+  providers: [KnexProvider, TaskGetByIdServiceProvider, ...defineResolvers()],
 })
 export class AppModule {}
