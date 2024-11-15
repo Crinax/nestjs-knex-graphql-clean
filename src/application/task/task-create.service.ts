f3cfc1e1e0bac619b94695f8589330227dcf2956
Crinax <agreedeth@gmail.com>
2024-11-15 20:08:43 +0300

    feat(task/services): add event publishing to services

--- src/application/task/task-create.service.ts
+++ src/application/task/task-create.service.ts
@@ -1,4 +1,6 @@
 import { TaskServiceMapper } from 'src/application/task/mappers';
+import { IEventBus } from 'src/core/abstracts/event-bus.abstract';
+import { TaskEvents } from 'src/core/events/task.events';
 import { TaskProps } from 'src/core/task/entities/taks.prop';
 import { TaskEntity } from 'src/core/task/entities/task.entity';
 import { CreateTaskCommand } from 'src/core/task/ports/primary/commands/create-task.command';
@@ -7,11 +9,21 @@ import { CreateTaskUseCase } from 'src/core/task/ports/primary/use-cases/create-
 import { TaskSavePort } from 'src/core/task/ports/secondary/task-save.port';
 
 export class TaskCreateService implements CreateTaskUseCase {
-  constructor(private readonly savePort: TaskSavePort) {}
+  constructor(
+    private readonly savePort: TaskSavePort,
+    private readonly eventBus?: IEventBus,
+  ) {}
 
   async create(command: CreateTaskCommand): Promise<TaskResponse> {
     const task = TaskProps.from(0, command.name);
     const saved = await this.savePort.save(new TaskEntity(task));
+
+    if (this.eventBus) {
+      this.eventBus.publish({
+        name: TaskEvents.TASK_CREATED,
+        data: saved,
+      });
+    }
 
     return TaskServiceMapper.toResponse(saved);
   }
--- src/application/task/task-update-name.service.ts
+++ src/application/task/task-update-name.service.ts
@@ -1,17 +1,29 @@
 import { TaskServiceMapper } from 'src/application/task/mappers';
+import { IEventBus } from 'src/core/abstracts/event-bus.abstract';
+import { TaskEvents } from 'src/core/events/task.events';
 import { UpdateTaskNameCommand } from 'src/core/task/ports/primary/commands/update-name.command';
 import { TaskResponse } from 'src/core/task/ports/primary/responses';
 import { UpdateTaskNameUseCase } from 'src/core/task/ports/primary/use-cases/update-task-name.use-case';
 import { TaskUpdateNamePort } from 'src/core/task/ports/secondary/task-update-name.port';
 
 export class TaskUpdateNameService implements UpdateTaskNameUseCase {
-  constructor(private readonly updateNamePort: TaskUpdateNamePort) {}
+  constructor(
+    private readonly updateNamePort: TaskUpdateNamePort,
+    private readonly eventBus?: IEventBus,
+  ) {}
 
   async update(command: UpdateTaskNameCommand): Promise<TaskResponse> {
     const result = await this.updateNamePort.updateName(
       command.id,
       command.name,
     );
+
+    if (this.eventBus) {
+      this.eventBus.publish({
+        name: TaskEvents.TASK_NAME_UPDATED,
+        data: result,
+      });
+    }
 
     return TaskServiceMapper.toResponse(result);
   }
